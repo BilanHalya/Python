07fa2442db8f94dec747f504269710f1d0452add
BilanHalya <43861298+BilanHalya@users.noreply.github.com>
2018-10-04 23:17:59 +0300

    Add files via upload

---
+++ Lab3.py
@@ -0,0 +1,27 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("LABA 3")
+LABA 3
+>>> print("MAde by Halyna Bilan")
+MAde by Halyna Bilan
+>>> print("subject - MATH")
+subject - MATH
+>>> print("Firstly, let`s try to calculate something")
+Firstly, let`s try to calculate something
+>>> from math import *
+>>> sqrt(16)
+4.0
+>>> pow(12,4)
+20736.0
+>>> print(pi)
+3.141592653589793
+>>> print('Let`s calculate out function')
+Let`s calculate out function
+>>> x=int(input('x='))
+x=5
+>>> z=int(input('z='))
+z=15
+>>> y=(2*pow(x,2)+x-5)/(x+2)+ cos(x/2*z)/sin(x/2*z)
+>>> print(y)
+2.1871008763291737
+>>> 
